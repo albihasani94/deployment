57d585b0ae745c199b6386edd5e8d23b3b562e89
albihasani94 <albinhasani@protonmail.com>
2024-11-30 18:49:30 +0100

    Initial deployment

---
+++ compose/V2_initial_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.organization
+VALUES (1, 'Ostock', 'Illary Huaylupo', 'illaryhs@gmail.com', '888888888');
+INSERT INTO public.organization
+VALUES (2, 'OptimaGrowth', 'Admin', 'illaryhs@gmail.com', '888888888');
+INSERT INTO public.organization
+VALUES (3, 'Ostock', 'Illary Huaylupo', 'illaryhs@gmail.com', '888888888');
+INSERT INTO public.license
+VALUES (1, 2, 'Software Product', 'Ostock',
+        'complete', 'I AM DEV');
+INSERT INTO public.license
+VALUES (2, 3, 'Software Product', 'Ostock',
+        'complete', 'I AM DEV');
